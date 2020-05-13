ba1d0f6b36fd5836f5b643a03229b86d42c6f7ba
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-05-13 16:53:06 +0530

    Numbered files | 150 down FInallygit add .

--- 99_numberFiles.py
+++ 99_numberFiles.py
@@ -4,9 +4,8 @@ files = os.listdir()
 # exclude .git and .gitignore and 0
 files.pop(0)
 files.pop(0)
-files.pop(0)
 ind = 1
 for file in files:
-    # print(f'{ind}_{file}')
-    os.system(f'cp {file} 0/{ind}_{file}')
+    print(f'mv {file} {ind}_{file}')
+    os.system(f'mv {file} {ind}_{file}')
     ind+=1
